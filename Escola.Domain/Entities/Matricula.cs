919b4cc54b34cc18aaf93f11b5de34914df3fdcb
Borges <borgespaulo72@yahoo.com.br>
2026-07-17 09:49:50 -0300

    feat: Iniciando os Entities

---
+++ Escola.Domain/Entities/Matricula.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Escola.Domain.Entities
+{
+    public class Matricula
+    {
+        public int Id { get; set; }
+        public int UsuarioId { get; set; }
+        public int TurmaId { get; set; }
+        public DateTime DataMatricula { get; set; } = DateTime.Now;
+        public DateTime DataExpiracao { get; set; } = DateTime.Now;
+        public bool Ativo { get; set; } = true;
+
+        // Matricula pode ter várias Notas, então é uma relação de 1 para N
+        public ICollection<Nota> Notas { get; set; }
+        public Usuario Usuario { get; set; } = new Usuario();
+        public Turma Turma { get; set; }
+    }
+}
---
+++ Escola.Domain/Entities/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Escola.Domain.Entities
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        //public string Senha { get; set; } nunca armazenar senha em texto puro, use hash e salt
+        public byte[] PasswordHash { get; set; }  // Armazena o hash da senha
+        public byte[] PasswordSalt { get; set; }  // pra verificar o passwordHash
+        public string Perfil { get; set; } // Ex: Admin, User, etc.
+
+        // usuario pode ter várias matriculas, então é uma relação de 1 para N
+        public ICollection<Matricula> Matriculas { get; set; }
+    }
+}
